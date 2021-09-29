ecf5f10705ba498e0f617993ca52979732e4dd8a
kyh196201 <kyh196201@gmail.com>
2021-09-29 18:30:39 +0900

    리다이렉트 시 router 이동하도록 수정

--- src/main.js
+++ src/main.js
@@ -13,3 +13,12 @@ new Vue({
   store,
   render: h => h(App),
 }).$mount('#app');
+
+const path = localStorage.getItem('path');
+
+if (path) {
+  localStorage.removeItem('path');
+  router.push({
+    path: path.replace('life-timer/', ''),
+  });
+}
